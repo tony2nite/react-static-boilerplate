b1f6d5a9ce4951d6868b62851fadbdabe58495b7
Anthony Hughes <tony2nite@gmail.com>
2016-08-02 08:59:40 +0200

    ESLint tidy

--- utils/prerender-file-writer.js
+++ utils/prerender-file-writer.js
@@ -1,33 +1,31 @@
 // var cache_manager = require('cache-manager');
-var fs = require('fs');
-var url = require('url');
-var mkdirp = require('mkdirp');
+const fs = require('fs');
+const url = require('url');
+const mkdirp = require('mkdirp');
 
 module.exports = {
-    setOutputPath: function(outputPath) {
-      this.outputPath = outputPath;
-    },
-    afterPhantomRequest: function(req, res, next) {
-        if(req.prerender.statusCode !== 200) {
-            return next();
+  setOutputPath(outputPath) {
+    this.outputPath = outputPath;
+  },
+
+  afterPhantomRequest(req, res, next) {
+    if (req.prerender.statusCode === 200) {
+      const path = this.outputPath + url.parse(req.prerender.url).pathname;
+      mkdirp(path, (err) => {
+        if (err) {
+          console.log('Error creating output folder', err);
+        } else {
+          let file;
+          if (path.substr(-1) === '/') {
+            file = `${path}index.html`;
+          } else {
+            file = `${path}/index.html`;
+          }
+          console.log(`Writing file ${file}`);
+          fs.writeFileSync(file, req.prerender.documentHTML, 'utf8');
         }
-
-        var path = this.outputPath + url.parse(req.prerender.url).pathname;
-        mkdirp(path, function (err) {
-            if (err) {
-              console.log('Error creating output folder', err);
-            } else {
-              var file
-              if (path.substr(-1) === '/') {
-                file = path + 'index.html';
-              } else {
-                file = path + '/index.html';
-              }
-              console.log('Writing file ' + file);
-              fs.writeFileSync(file, req.prerender.documentHTML, 'utf8');
-
-              return next();
-            }
-        });
+      });
     }
+    return next();
+  },
 };
